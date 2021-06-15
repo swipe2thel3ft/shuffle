b71ab1a2baed2fb61ad6e3c8216704ffc83c2e5a
Victor Perez <victorperez099@yahoo.com>
2021-06-14 21:23:52 -0600

    Homework overload

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -2,10 +2,18 @@ package edu.cnm.deepdive;
 
 import java.util.Random;
 
+/**
+ * Implements <a href="https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle">Fisher-Yates</a> shuffle for all primitive data types as well as {@link Object}.
+ */
 public class Shuffler {
 
   private final Random rng;
 
+  /**
+   * Initializes this instance, using the provided source of randomness.
+   *
+   * @param rng source of randomness
+   */
   public Shuffler(Random rng) {
     this.rng = rng;
   }
@@ -18,4 +26,78 @@ public class Shuffler {
       data[sourceIndex] = temp;
     }
   }
+
+  public void shuffle(byte[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      byte temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(Object[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      Object temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(short[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      short temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(char[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      char temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(long[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      long temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(float[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      float temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(double[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      double temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(boolean[] data) {
+    for (int targetIndex = data.length  - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      boolean temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
 }
+
